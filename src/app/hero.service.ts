e76f4f057e6bd1c2f89707cf5dded8f7cca8a9be
rafael.resende <rafael.resende@lynxprocess.com>
2019-03-18 15:33:51 -0300

    modifications errors file hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,7 +7,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
-const HttpOptions = {
+const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
@@ -17,6 +17,8 @@ const HttpOptions = {
   providedIn: 'root'
 })
 export class HeroService {
+
+  private heroesUrl = 'api/heroes'; // URL to web api.
 
   constructor(
     private http: HttpClient,
@@ -98,9 +100,11 @@ export class HeroService {
   /**
    * Handle Http operation that failed.
    * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
    */
+   //@param operation - name of the operation that failed
+   //@param result - optional value to return as the observable result
+
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
